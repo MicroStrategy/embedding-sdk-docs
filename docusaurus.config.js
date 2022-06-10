7f319875d4123d4b37d0d55e095d3d044a36094d
Liang, Jiapei <jliang@microstrategy.com>
2022-06-10 17:49:55 -0400

    updated editUrl

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            "https://github.microstrategy.com/jliang/embedding-sdk-docs-docusaurus",
+            "https://github.microstrategy.com/jliang/embedding-sdk-docs-docusaurus/blob/main/",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
