ffb06487604d871a3ce1a08dc86a1a3eb8e97c7b
Liang, Jiapei <jliang@microstrategy.com>
2022-06-10 17:31:32 -0400

    added local search feature

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -75,7 +75,31 @@ const config = {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
       },
+      // algolia: {
+      //   // The application ID provided by Algolia
+      //   appId: "VNIM4BJNXH",
+
+      //   // Public API key: it is safe to commit it
+      //   apiKey: "63b4dee06cd578c7d072be3818dbf145",
+
+      //   indexName: "embedding-sdk-docs",
+
+      //   // Optional: see doc section below
+      //   contextualSearch: true,
+
+      //   // Optional: Specify domains where the navigation should occur through window.location instead on history.push. Useful when our Algolia config crawls multiple documentation sites and we want to navigate with window.location.href to them.
+      //   externalUrlRegex: "external\\.com|domain\\.com",
+
+      //   // Optional: Algolia search parameters
+      //   searchParameters: {},
+
+      //   // Optional: path for search page that enabled by default (`false` to disable it)
+      //   searchPagePath: "search",
+
+      //   //... other Algolia params
+      // },
     }),
+  plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
 };
 
 module.exports = config;
