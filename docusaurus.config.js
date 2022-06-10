1ab5712b49ff935dadc28625edba8ccb5627c8c0
Liang, Jiapei <jliang@microstrategy.com>
2022-06-10 16:52:25 -0400

    modified url and baseUrl config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,8 +8,8 @@ const darkCodeTheme = require("prism-react-renderer/themes/dracula");
 const config = {
   title: "My Site",
   tagline: "Dinosaurs are cool",
-  url: "https://your-docusaurus-test-site.com",
-  baseUrl: "/",
+  url: "https://github.microstrategy.com/",
+  baseUrl: "/pages/jliang/embedding-sdk-docs-docusaurus/",
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
   favicon: "img/favicon.ico",
