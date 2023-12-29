6a56d11eb181ff0595827a802198f65e1caf184c
ZiyuHe <zhe@microstrategy.com>
2023-12-29 10:56:20 +0800

    US531457; change api doc version from 2021 to 2024

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -80,7 +80,7 @@ const config = {
           },
           {
             type: "dropdown",
-            label: "Version 2021",
+            label: "Version 2024",
             position: "right",
             items: [
               {
